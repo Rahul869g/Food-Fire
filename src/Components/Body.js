b84b87d318f422210f784d775ac282ff1ea1c53c
Rahul Gupta <rahul123alok@gmail.com>
2023-10-06 23:45:29 +0530

    updated body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -48,45 +48,49 @@ const Body = () => {
 
   return (
     <>
-      <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search a restaurant you want..."
-          value={searchText}
-          // update the state variable searchText when we typing in input box
-          onChange={(e) => setSearchText(e.target.value)}
-        ></input>
-        <button
-          data-testid="search-container"
-          className="search-btn"
-          onClick={() => {
-            // user click on button searchData function is called
-            searchData(searchText, allRestaurants);
-          }}
-        >
-          Search
-        </button>
-      </div>
-      {errorMessage && <div className="error-container">{errorMessage}</div>}
-
-      {/* if restaurants data is not fetched then display Shimmer UI after the fetched data display restaurants cards */}
       {allRestaurants?.length === 0 ? (
         <Shimmer />
       ) : (
-        <div className="restaurant-list" data-testid="res-list">
-          {/* We are mapping restaurants array and passing JSON array data to RestaurantCard component as props with unique key as restaurant.data.id */}
-          {filteredRestaurants.map((restaurant) => {
-            return (
-              <Link
-                to={"/restaurant/" + restaurant?.info?.id}
-                key={restaurant.info.id}
-              >
-                <RestaurantCard {...restaurant?.info} />
-              </Link>
-            );
-          })}
-        </div>
+        <>
+          <div className="search-container">
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search a restaurant you want..."
+              value={searchText}
+              // update the state variable searchText when we typing in input box
+              onChange={(e) => setSearchText(e.target.value)}
+            ></input>
+            <button
+              data-testid="search-btn"
+              className="search-btn"
+              onClick={() => {
+                // user click on button searchData function is called
+                searchData(searchText, allRestaurants);
+              }}
+            >
+              Search
+            </button>
+          </div>
+          {errorMessage && (
+            <div className="error-container">{errorMessage}</div>
+          )}
+
+          {/* if restaurants data is not fetched then display Shimmer UI after the fetched data display restaurants cards */}
+          <div className="restaurant-list" data-testid="res-list">
+            {/* We are mapping restaurants array and passing JSON array data to RestaurantCard component as props with unique key as restaurant.data.id */}
+            {filteredRestaurants.map((restaurant) => {
+              return (
+                <Link
+                  to={"/restaurant/" + restaurant?.info?.id}
+                  key={restaurant.info.id}
+                >
+                  <RestaurantCard {...restaurant?.info} />
+                </Link>
+              );
+            })}
+          </div>
+        </>
       )}
     </>
   );
